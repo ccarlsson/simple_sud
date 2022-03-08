56be1dd0a07fc6c497ddc8f0053d3c7c6394fce5
Christer Carlsson <ccarlsson@outlook.com>
2022-03-08 15:34:23 +0100

    Add project files.

---
+++ simple_sud/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace simple_sud
+{
+    class Program
+    {
+
+        public static void Main()
+        {
+            Console.WriteLine("SUD, World!");
+            // Build the map
+
+            Room room1 = new Room { Description = "Room 1" };
+            Room room2 = new Room { Description = "Room 2" };
+            Room room3 = new Room { Description = "Room 3" };
+            Room room4 = new Room { Description = "Room 4" };
+            Room room5 = new Room { Description = "Room 5" };
+            Room room6 = new Room { Description = "Room 6" };
+            Room room7 = new Room { Description = "Room 7" };
+
+            room1.AddToNorth(room2);
+            room1.AddToEast(room4);
+            room4.AddToEast(room5);
+            room5.AddToNorth(room6);
+            room5.AddToSouth(room7);
+            room2.AddWest(room3);
+
+            // Add Startroom
+            Room currentRoom = room1;
+
+            // Gameloop
+            while (true)
+            {
+                Console.Write("Where do you want to go? (n, s, e, w) : ");
+                var keyInfo = Console.ReadKey(true);
+
+                Room tmpRoom = null;
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.N:
+                        tmpRoom = currentRoom.GoNorth;
+                        break;
+                    case ConsoleKey.S:
+                        tmpRoom = currentRoom.GoSouth;
+                        break;
+                    case ConsoleKey.E:
+                        tmpRoom = currentRoom.GoEast;
+                        break;
+                    case ConsoleKey.W:
+                        tmpRoom = currentRoom.GoWest;
+                        break;
+                    default:
+                        System.Console.Write("I don't understand that, and ");
+                        break;
+                }
+
+                if (tmpRoom == null)
+                {
+                    System.Console.WriteLine("you can't go that direction");
+                    continue;
+                }
+                currentRoom = tmpRoom;
+                Console.WriteLine($"You are in {currentRoom.Description}");
+            }
+
+        }
+    }
+}
---
+++ simple_sud/Room.cs
@@ -0,0 +1,48 @@
+﻿namespace simple_sud
+{
+    public class Room
+    {
+        private Room _north;
+        private Room _south;
+        private Room _east;
+        private Room _west;
+
+        public string Description { get; set; } = "";
+
+        public Room GoNorth
+        {
+            get
+            {
+                return _north;
+            }
+        }
+
+        public Room? GoSouth => _south;
+        public Room? GoEast => _east;
+        public Room? GoWest => _west;
+
+        public void AddToNorth(Room otherRoom)
+        {
+            otherRoom._south = this;
+            _north = otherRoom;
+        }
+        public void AddToSouth(Room otherRoom)
+        {
+            otherRoom._north = this;
+            _south = this;
+        }
+
+        public void AddToEast(Room otherRoom)
+        {
+            otherRoom._west = this;
+            _east = otherRoom;
+        }
+
+        public void AddWest(Room otherRoom)
+        {
+            otherRoom._east = this;
+            _west = otherRoom;
+        }
+
+    }
+}
